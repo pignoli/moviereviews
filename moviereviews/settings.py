3eca9fa2cac6adb5a74914dd488e8c3a895fc35b
WM <wmishima@gmail.com>
2022-03-21 08:41:53 +0900

    chapter 10

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,3 +113,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('about/', movieViews.about, name='about'),
     path('signup/', movieViews.signup, name='signup'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
