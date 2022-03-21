6cb1b3649b4fcf17dacee390666f22dc728d59df
WM <wmishima@gmail.com>
2022-03-21 18:17:08 +0900

    chap 19-4

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreateForm
+
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.shortcuts import redirect
@@ -9,7 +11,7 @@ from django.db import IntegrityError
 def signupaccount(request):
     if request.method == 'GET':
         return render(request, 'signupaccount.html',
-            {'form':UserCreationForm})
+            {'form':UserCreateForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
@@ -20,11 +22,11 @@ def signupaccount(request):
                 return redirect('home')
             except IntegrityError:
                 return render(request, 'signupaccount.html',
-                {'form':UserCreationForm,
+                {'form':UserCreateForm,
                 'error':'Username already taken. Choose new username.'})
         else:
             return render(request, 'signupaccount.html',
-            {'form':UserCreationForm, 'error':'Passwords do not match'})
+            {'form':UserCreateForm, 'error':'Passwords do not match'})
 
 
 
