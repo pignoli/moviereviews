99b7e99897f80cee43e6d58c38e5693ceefb7576
WM <wmishima@gmail.com>
2022-03-21 11:57:46 +0900

    chap 16

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -117,3 +118,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+BASE_DIR / 'moviereviews/static/',
+]
