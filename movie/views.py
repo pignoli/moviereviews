b9ffecf19f6bff9a34dcd265a75e4d5637cd7e9a
WM <wmishima@gmail.com>
2022-03-21 09:06:54 +0900

    chapter 11-1

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'searchTerm':searchTerm})
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
