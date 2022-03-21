6f94ceeea39ec5e5069c5b1b495b3f16a4ea0447
WM <wmishima@gmail.com>
2022-03-21 12:02:40 +0900

    Revert "chap 16"
    This reverts commit 99b7e99897f80cee43e6d58c38e5693ceefb7576.

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -110,7 +110,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -118,7 +117,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = [
-BASE_DIR / 'moviereviews/static/',
-]
