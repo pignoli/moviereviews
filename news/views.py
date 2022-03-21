297225eebf7a60dff6e3e65cb8c0ac1c3826984c
WM <wmishima@gmail.com>
2022-03-21 10:02:58 +0900

    chapter 12-5

--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import News
 
 def news(request):
-    newss = News.objects.all()
+    newss = News.objects.all().order_by('-date')
     return render(request, 'news.html', {'newss':newss})
 
 
