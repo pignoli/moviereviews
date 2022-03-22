ac16122ccc3a8ad9aa6dd792460af03fbf970e9a
WM <wmishima@gmail.com>
2022-03-22 21:41:08 +0900

    chap 22

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .forms import UserCreateForm
 
 from django.contrib.auth.models import User
-from django.contrib.auth import login,logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
 
@@ -30,6 +31,22 @@ def signupaccount(request):
 def logoutaccount(request):
     logout(request)
     return redirect('home')
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html',
+        {'form':AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'],
+        password=request.POST['password'])
+        if user is None:
+            return render(request,'loginaccount.html',
+                {'form': AuthenticationForm(),
+            'error': 'username and password do not match'})
+        else:
+            login(request,user)
+            return redirect('home')
+
 
 
 
