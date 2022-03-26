8cf5d380a46741cb0f4be248b16aa7e909e34179
WM <wmishima@gmail.com>
2022-03-26 15:56:15 +0900

    chap 24

--- movie/urls.py
+++ movie/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<int:movie_id>', views.detail, name='detail'),
+    path('<int:movie_id>/create', views.createreview,
+        name='createreview'),
+
 ]
