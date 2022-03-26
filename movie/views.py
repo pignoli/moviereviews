bacc9903ec3d117c3ad9b947d4050dad57cf4ddf
WM <wmishima@gmail.com>
2022-03-26 16:43:06 +0900

    chap 25

--- movie/views.py
+++ movie/views.py
@@ -21,7 +21,8 @@ def signup(request):
 
 def detail(request, movie_id):
     movie = get_object_or_404(Movie,pk=movie_id)
-    return render(request, 'detail.html', {'movie':movie})
+    reviews=Review.objects.filter(movie=movie)
+    return render(request, 'detail.html', {'movie':movie,'reviews':reviews})
 
 def createreview(request, movie_id):
     movie = get_object_or_404(Movie,pk=movie_id)
